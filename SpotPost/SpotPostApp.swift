a02411935dcde58a44c8c7d62f8b5ef925ddc598
okaki1999 <okakilikecat@gmail.com>
2025-01-12 03:06:58 +0900

    Initial Commit

---
+++ SpotPost/SpotPostApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotPostApp.swift
+//  SpotPost
+//
+//  Created by fujita taiki on 2025/01/12.
+//
+
+import SwiftUI
+
+@main
+struct SpotPostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
